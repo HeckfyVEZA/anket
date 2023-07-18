c2e4db595c57e04b7954a5ffacf2482203ff31ee
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-07-18 10:10:26 +0300

    Add files via upload

--- anketa.py
+++ anketa.py
@@ -1,3 +1,14 @@
 import streamlit as st
-import sys
-st.write(sys.version)
+try:
+    flag = st.session_state.flag
+except:
+    flag = False
+st.session_state['stage'] = 0
+if st.session_state['stage'] == 0:
+    st.write('hui')
+cb = st.checkbox('dfhgjdhfg')
+if cb:
+    st.session_state['stage'] = 2
+    st.session_state.flag = True
+else:
+    st.session_state.flag = False
