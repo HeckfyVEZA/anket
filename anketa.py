1a4d1f433766138e11cb62e69f2be489854a0932
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-07-18 10:11:56 +0300

    Add files via upload

--- anketa.py
+++ anketa.py
@@ -1,14 +1,16 @@
 import streamlit as st
 try:
-    flag = st.session_state.flag
+    flag = st.session_state['flag']
 except:
     flag = False
-st.session_state['stage'] = 0
+    st.session_state['stage'] = 0
+
 if st.session_state['stage'] == 0:
     st.write('hui')
 cb = st.checkbox('dfhgjdhfg')
 if cb:
     st.session_state['stage'] = 2
-    st.session_state.flag = True
+    st.session_state['flag'] = True
 else:
-    st.session_state.flag = False
+    st.session_state['flag'] = False
+st.session_state
